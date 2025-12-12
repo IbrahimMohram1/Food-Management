e07810ee74c18ff57282720b454ed67037f9420d
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2025-12-12 14:43:51 +0200

    style: foramt code with prettier

--- src/Hooks/useAuth.js
+++ src/Hooks/useAuth.js
@@ -1,29 +1,27 @@
 import axios from "axios";
 import axiosClient from "../Utils/axiosClient";
 
-export const useAuthApi=()=>{
-    const login = async (values)=>{
-        const {data} = await axiosClient.post('/Users/Login' , values)
-        return data
-    }
-       const register = async (values)=>{
-        const {data} = await axiosClient.post('/Users/Register' , values)
-        return data
-    }
-         const forgetPassword = async (values)=>{
-        const {data} = await axiosClient.post('/Users/Reset/Request' , values)
-        return data
-    }
-         const resetPassword = async (values)=>{
-        const {data} = await axiosClient.post('/Users/Reset' , values)
-        return data
-    }
-            const verifyAccount = async (values)=>{
-        const {data} = await axiosClient.post('/Users/verify' , values)
-        return data
-    }
+export const useAuthApi = () => {
+  const login = async (values) => {
+    const { data } = await axiosClient.post("/Users/Login", values);
+    return data;
+  };
+  const register = async (values) => {
+    const { data } = await axiosClient.post("/Users/Register", values);
+    return data;
+  };
+  const forgetPassword = async (values) => {
+    const { data } = await axiosClient.post("/Users/Reset/Request", values);
+    return data;
+  };
+  const resetPassword = async (values) => {
+    const { data } = await axiosClient.post("/Users/Reset", values);
+    return data;
+  };
+  const verifyAccount = async (values) => {
+    const { data } = await axiosClient.post("/Users/verify", values);
+    return data;
+  };
 
-
-
-    return {login , register , forgetPassword , resetPassword , verifyAccount}
-}
+  return { login, register, forgetPassword, resetPassword, verifyAccount };
+};
--- src/Utils/axiosClient.js
+++ src/Utils/axiosClient.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
-
-
 const axiosClient = axios.create({
-    baseURL:`https://upskilling-egypt.com:3006/api/v1`,
-    withCredentials:false,
-
-
-})
+  baseURL: `https://upskilling-egypt.com:3006/api/v1`,
+  withCredentials: false,
+});
 
 export default axiosClient;
