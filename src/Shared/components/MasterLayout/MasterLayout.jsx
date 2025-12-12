d9a775756dbe5aa00bfac9f1fc1096ff9dd06ac5
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2025-12-12 17:47:10 +0200

    Update : Add Auth Store

--- src/Shared/components/MasterLayout/MasterLayout.jsx
+++ src/Shared/components/MasterLayout/MasterLayout.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import Sidebar from '../Sidebar/Sidebar.jsx'
-import Navbar from '../Navbar/Navbar.jsx'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Sidebar from "../Sidebar/Sidebar.jsx";
+import Navbar from "../Navbar/Navbar.jsx";
+import { Outlet } from "react-router-dom";
 
 export default function MasterLayout() {
   return (
-  <>
-    <div className="d-flex vh-100">
-    <div>
-    <Sidebar/>
-    </div>
-    <div className='w-100'>
-        <Navbar/>
-        <Outlet/>
-
-    </div>
-    </div>
-  
-  </>
-  )
+    <>
+      <div className="d-flex vh-100">
+        <div>
+          <Sidebar />
+        </div>
+        <div className="w-100">
+          <Navbar />
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/store/authStore.js
@@ -0,0 +1,58 @@
+import { create } from "zustand";
+import { useAuthApi } from "../Hooks/useAuth";
+
+export const useAuthStore = create((set) => {
+  const { login, register, forgetPassword, resetPassword, verifyAccount } =
+    useAuthApi();
+  return {
+    loading: false,
+    error: null,
+    token: null,
+    LoginUser: async (data, toast, navigate) => {
+      try {
+        const response = await login(data);
+        toast.success("Login is Success");
+        navigate("/dashboard");
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    },
+    RegisterUser: async (data, toast, navigate) => {
+      try {
+        const response = await register(data);
+        toast.success(response.data.message);
+        navigate("/verify-account");
+      } catch (error) {
+        toast.error(error?.response?.data?.message);
+      }
+    },
+    ForgetUserPassword: async (data, toast, navigate) => {
+      try {
+        const response = await forgetPassword(data);
+
+        toast.success(response.message);
+        navigate("/reset-pass");
+      } catch (error) {
+        toast.error(error?.response?.data?.message);
+      }
+    },
+    ResetUserPassword: async (data, toast, navigate) => {
+      try {
+        const response = await resetPassword(data);
+        toast.success(response.message);
+        navigate("/login");
+      } catch (error) {
+        toast.error(error?.response?.data?.message);
+      }
+    },
+    VerifyAcc: async (data, toast, navigate) => {
+      try {
+        const response = await verifyAccount(data);
+        toast.success(response.message);
+        navigate("/login");
+      } catch (error) {
+        toast.error(error?.response?.data?.message);
+      }
+    },
+  };
+});
