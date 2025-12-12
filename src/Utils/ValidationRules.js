83e58aaac276785fab85e6fad486405fe6f962d6
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2025-12-12 02:45:49 +0200

    feat: refactor Validation Rules

--- src/Utils/ValidationRules.js
+++ src/Utils/ValidationRules.js
@@ -1,57 +1,51 @@
+export const userNameRules = {
+  required: "UserName is Required",
+  maxLength: { value: 8, message: "Maximum length is 8" },
+  pattern: {
+    value: /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{1,8}$/,
+    message: "Please Enter a Char with number max is 8",
+  },
+};
+export const emailRules = {
+  required: "Email is Required",
+  pattern: {
+    value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
+    message: "Please Enter a Valid Mail",
+  },
+};
 
+export const passwordRules = {
+  required: "Password is Required",
+  pattern: {
+    value: /^(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&*!])[A-Za-z\d@#$%^&*!]{5,}$/,
+    message:
+      "Password must start with a capital letter and contain letters, numbers, and a special character",
+  },
+};
 
-export const userNameRules={
-        required: "UserName is Required",
-                    maxLength: {
-                      value: 8,
-                      message: "Maximum length is 8",
-                    },
-                    pattern: {
-                      value: /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{1,8}$/,
-                      message: "Please Enter a Char with number max is 8",
-                    },
-}
-export const emailRules ={
-    required: "Email is Required",
-              pattern: {
-                value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
-                message: "Please Enter a Valid Mail",
-              },
-}
+export const countryRules = {
+  required: "Country is Required",
+  minLength: {
+    value: 3,
+    message: "min length is 3",
+  },
+};
+export const phoneRules = {
+  required: "PhoneNumber is Required",
+  pattern: {
+    value: /^(?:\+20)?01[0-9]{9}$/,
+    message: "Please enter a valid Phone Number",
+  },
+};
+export const otpRules = {
+  required: "OTP is Required",
+  minLength: {
+    value: 3,
+    message: "Enter a Valid OTP Code",
+  },
+};
 
-export const passwordRules ={
-         required: "Password is Required",
-              pattern: {
-                value:
-                  /^(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&*!])[A-Za-z\d@#$%^&*!]{5,}$/,
-                message:
-                  "Password must start with a capital letter and contain letters, numbers, and a special character",
-              },
-}
-
-export const countryRules={
-      required: "Country is Required",
-                    minLength: {
-                      value: 3,
-                      message: "min length is 3",
-                    },
-}
-export const phoneRules={
-       required: "PhoneNumber is Required",
-                    pattern: {
-                      value: /^(?:\+20)?01[0-9]{9}$/,
-                      message: "Please enter a valid Phone Number",
-                    },
-}
-export const confirmPasswordRules =(password)=>( {
+export const confirmPasswordRules = (password) => ({
   required: "Confirm Password is required",
-    validate: (value) => value === password || "Passwords do not match",
-})
-
-export const otpRules={
-     required: "OTP is Required",
-                minLength: {
-                  value: 3,
-                  message: "Enter a Valid OTP Code",
-                },
-}
+  validate: (value) => value === password || "Passwords do not match",
+});
