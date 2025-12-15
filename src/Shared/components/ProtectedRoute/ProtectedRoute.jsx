4eaf25e77f7757a9af4d2f72fa10ffe859c43518
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2025-12-15 03:52:51 +0200

    feat: add protected route using valid token

--- src/Shared/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/Shared/components/ProtectedRoute/ProtectedRoute.jsx
@@ -4,7 +4,9 @@ import { Navigate } from "react-router-dom";
 export default function ProtectedRoute({ children }) {
   const token = localStorage.getItem("access_token");
   if (!token) {
-    return <Navigate to="/login" replace />; // لو مفيش توكن رجع login
+    return <Navigate to="/login" replace />;
+  } else {
+    return <Navigate to="/dashboard" replace />;
   }
 
   return children;
