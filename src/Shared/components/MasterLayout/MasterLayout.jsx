2d7a50f9f2aaa7e33683af5e71a636ae85eb72f1
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2025-12-21 03:39:29 +0200

    fix:SideBar Postion

--- src/Shared/components/MasterLayout/MasterLayout.jsx
+++ src/Shared/components/MasterLayout/MasterLayout.jsx
@@ -6,8 +6,8 @@ import AppSideBar from "../AppSideBar/AppSideBar.jsx";
 export default function MasterLayout() {
   return (
     <>
-      <div className="d-flex vh-100">
-        <div className="vh-100  ">
+      <div className="d-flex ">
+        <div className=" sideBar ">
           <AppSideBar />
         </div>
         <div className="  w-100   ps-3 ">
